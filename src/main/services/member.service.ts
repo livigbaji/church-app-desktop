9950426ffd6ffa49b674d355ec66836c77681f5e
Livinus Igbaji <bestbrain10@gmail.com>
2024-03-07 17:31:46 +0100

    add member methods for birthdays, registration, suspensions and updates

--- src/main/services/member.service.ts
+++ src/main/services/member.service.ts
@@ -2,7 +2,7 @@ import {FindManyOptions, Like, Repository} from "typeorm";
 import {ipcMain} from "@electron/remote";
 import {Member} from "../models/member.model.ts";
 import {ExternalMembers} from "../models/external-members.model.ts";
-import {ListMembersRequest} from "../types";
+import {ListMembersRequest, MemberStatus} from "../types";
 
 export class MemberService {
     constructor(
@@ -36,5 +36,56 @@ export class MemberService {
         return model.find(query)
     }
 
+    createMember(member: Partial<Member>) {
+        return this.memberRepo.save(this.memberRepo.create(member));
+    }
 
+    updateMember(id: string, member: Partial<Member>) {
+        return this.memberRepo.update({
+            id,
+        }, member)
+    }
+
+    createExternalMember(member: Partial<ExternalMembers>) {
+        return this.externalMemberRepo.save(this.externalMemberRepo.create(member));
+    }
+
+    updateExternalMember(id: string, member: Partial<ExternalMembers>) {
+        return this.externalMemberRepo.update({
+            id,
+        }, member)
+    }
+
+    deleteMember(id: string, isExternal: boolean) {
+        const model = isExternal ? this.externalMemberRepo : this.memberRepo;
+
+        return model.delete({ id });
+    }
+
+    suspendMember(id: string, note: string) {
+        return this.memberRepo.update({
+            id,
+        }, {
+            status: MemberStatus.SUSPENDED,
+            suspensionDescription: note,
+            suspendedAt: new Date(),
+        })
+    }
+
+    unsuspend(id: string) {
+        return this.memberRepo.update({
+            id,
+        }, {
+            status:  MemberStatus.ACTIVE,
+            suspendedAt: null,
+        });
+    }
+
+    birthdays(month: number) {
+        return this.memberRepo.find({
+            where: {
+                birthMonth: month
+            }
+        });
+    }
 }
