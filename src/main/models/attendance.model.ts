7b0e1a1d03a4dc772f8f951a3cfc4293e4661bbf
Livinus Igbaji <bestbrain10@gmail.com>
2024-03-09 23:58:43 +0100

    rewrite models in objection

--- src/main/models/attendance.model.ts
+++ src/main/models/attendance.model.ts
@@ -1,55 +1,26 @@
-import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {Model, snakeCaseMappers} from 'objection';
+import {ATTENDANCE_TABLE} from "../configs/constants.ts";
 import {AttendanceStatus, UserType} from "../types";
 
 
-@Entity()
-export class Attendance {
+export class Attendance extends Model {
+    static idColumn = 'id';
+    static get tableName() {
+        return ATTENDANCE_TABLE
+    }
 
-    @PrimaryGeneratedColumn('uuid')
+    static get columnNameMappers() {
+        return snakeCaseMappers();
+    }
+
     id!: string;
-
-    @Column({
-        name: 'user_id'
-    })
     user!: string;
-
-    @Column({
-        enum: UserType,
-        name: 'user_type'
-    })
     userType!: UserType;
-
-
-    @Column({
-        name: 'time_in'
-    })
     timeIn!: Date;
-
-    @Column({
-        nullable: true,
-        name: 'time_out'
-    })
     timeOut?: Date;
-
-    @Column({
-        enum: AttendanceStatus
-    })
     status!: AttendanceStatus;
-
-    @Column({
-        nullable: true,
-    })
     deleted?: boolean;
-
-    @Column({
-        name: 'deleted_at',
-        nullable: true,
-    })
     deletedAt?: Date;
-
-    @CreateDateColumn()
     createdAt!: Date;
-
-    @UpdateDateColumn()
     updatedAt!: Date;
 }
