9ef86316cd41c649f7d4e110e93136a04b9f16a1
Gideon-Buba <bubaambore@gmail.com>
2024-03-12 23:18:12 +0100

    Working on pages UI

--- src/renderer/components/Dashboard.tsx
+++ src/renderer/components/Dashboard.tsx
@@ -1,14 +1,58 @@
-import "../styles/Dashboard.css"
+import React from "react";
+import "../styles/Dashboard.css";
 import Header from "./Header";
+import { BsPeople, BsCollection, BsXCircle, BsCalendar } from "react-icons/bs";
 
+const fakeDashboardData = [
+  {
+    id: 1,
+    icon: BsPeople,
+    numb: "35",
+    text: "TOTAL MEMBERS",
+  },
+  {
+    id: 2,
+    icon: BsCollection,
+    numb: "08",
+    text: "TOTAL SUB-UNITS",
+  },
+  {
+    id: 3,
+    icon: BsXCircle,
+    numb: "05",
+    text: "ABSENT LAST WEEK",
+  },
+  {
+    id: 4,
+    icon: BsCalendar,
+    numb: "02",
+    text: "UPCOMING BIRTHDAYS",
+  },
+];
 
-const Dashbaord: React.FC = () => {
-  return(
+const Dashboard: React.FC = () => {
+  return (
     <div className="container">
       <Header pageTitle="Dashboard" />
-      <p>Content will go here</p>
+      <div className="row">
+        {fakeDashboardData.map((data) => (
+          <div className="col-md-3 mb-4" key={data.id}>
+            <div className="card h-100 border-dark">
+              <div className="card-body d-flex align-items-center">
+                <div className="dashboard-card-icon mr-2">
+                  <data.icon />
+                </div>
+                <div className="dashboard-card-text flex-grow-1"> 
+                  <p className="m-0 font-weight-bold">{data.numb}</p>
+                  <p className="m-0">{data.text}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashbaord;
+export default Dashboard;
