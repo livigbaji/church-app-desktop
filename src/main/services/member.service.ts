6a14cfcc91adadc6cc802eac7518466b4d7bf1c3
Livinus Igbaji <bestbrain10@gmail.com>
2024-03-20 22:59:05 +0100

    add IPC listener for member service

--- src/main/services/member.service.ts
+++ src/main/services/member.service.ts
@@ -2,6 +2,7 @@ import {ipcMain} from "@electron/remote";
 import {Member} from "../models/member.model.ts";
 import {ExternalMembers} from "../models/external-members.model.ts";
 import {ListMembersRequest, MemberStatus} from "../types";
+import IpcMainInvokeEvent = Electron.IpcMainInvokeEvent;
 
 export class MemberService {
     constructor() {
@@ -9,12 +10,48 @@ export class MemberService {
     }
 
     init() {
-        ipcMain.handle('get:members',  () => {
-            return [] // this.listMembers();
+        ipcMain.handle('get:members',  (_event: IpcMainInvokeEvent, request: ListMembersRequest) => {
+            return this.listMembers(request);
         });
 
-        ipcMain.handle('get:external-members',  () => {
-            return [] // this.listExternalMembers();
+        ipcMain.handle('get:external-members',  (_event: IpcMainInvokeEvent, request: ListMembersRequest) => {
+            return this.listExternalMembers(request);
+        });
+
+        ipcMain.handle('create:member',  (_event: IpcMainInvokeEvent, member: Partial<Member>) => {
+            return this.createMember(member);
+        });
+
+        ipcMain.handle('update:member',  (_event: IpcMainInvokeEvent, id: string, member: Partial<Member>) => {
+            return this.updateMember(id, member);
+        });
+
+        ipcMain.handle('delete:member',  (_event: IpcMainInvokeEvent, id: string) => {
+            return this.deleteMember(id);
+        });
+
+        ipcMain.handle('create:external-member',  (_event: IpcMainInvokeEvent, member: Partial<ExternalMembers>) => {
+            return this.createExternalMember(member);
+        });
+
+        ipcMain.handle('update:external-member',  (_event: IpcMainInvokeEvent, id: string, member: Partial<ExternalMembers>) => {
+            return this.updateExternalMember(id, member);
+        });
+
+        ipcMain.handle('delete:external-member',  (_event: IpcMainInvokeEvent, id: string) => {
+            return this.deleteExternalMember(id);
+        });
+
+        ipcMain.handle('suspend:member',  (_event: IpcMainInvokeEvent, id: string, note: string) => {
+            return this.suspendMember(id, note);
+        });
+
+        ipcMain.handle('unsuspend:member',  (_event: IpcMainInvokeEvent, id: string) => {
+            return this.unsuspend(id);
+        });
+
+        ipcMain.handle('birthdays:member',  (_event: IpcMainInvokeEvent, month: number) => {
+            return this.birthdays(month);
         });
     }
 
