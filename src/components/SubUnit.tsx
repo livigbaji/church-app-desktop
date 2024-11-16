ea02c9ce64cd3583c2785ad9697be855b5b5c1b9
Gideon-Buba <bubaambore@gmail.com>
2024-11-16 01:37:38 +0100

    wip: display subunit table

--- src/components/SubUnit.tsx
+++ src/components/SubUnit.tsx
@@ -1,18 +1,114 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Icon,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { getUnits } from "@/services/unitService";
+import { UnitData } from "@/types";
 import CustomSpeedDial from "./CustomSpeedDial";
 import Header from "./Header";
+import CsvUpload from "./CsvUpload";
+
+const AddUnitIcon = (
+  <Icon
+    sx={{
+      height: "30px",
+      width: "30px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <img src="/public/assets/icons/add-unit-icon.svg" alt="Add Unit" />
+  </Icon>
+);
 
 const SubUnit: React.FC = () => {
+  const [units, setUnits] = useState<UnitData[]>([]);
+
+  useEffect(() => {
+    const fetchUnits = async () => {
+      try {
+        const data = await getUnits();
+        setUnits(data);
+      } catch (error) {
+        console.error("Error fetching units:", error);
+      }
+    };
+
+    fetchUnits();
+  }, []);
+
   return (
     <div className="container">
       <Header pageTitle="Sub-Units" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nemo,
-        molestias dolore cumque deleniti ipsam tempore quae quasi est, aliquam,
-        beatae assumenda eligendi sit accusamus! Provident numquam reiciendis
-        ullam quibusdam vitae possimus facilis rem. Neque qui assumenda veniam
-        aspernatur, expedita minus nihil accusantium. Incidunt cupiditate quam
-        voluptatum aperiam harum? Ea!
-      </p>
+
+      <Box
+        sx={{
+          mt: 2,
+          bgcolor: "#fff",
+          display: "flex",
+          justifyContent: "flex-end",
+          gap: 2,
+        }}
+      >
+        <CsvUpload
+          onFileSelected={function (file: File): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+
+        <Button
+          startIcon={AddUnitIcon}
+          variant="contained"
+          disableElevation
+          sx={{
+            mb: 2,
+            height: 56,
+            minWidth: 180,
+            whiteSpace: "nowrap",
+            borderRadius: "5px",
+            bgcolor: "#132034",
+            textTransform: "none",
+            fontSize: "14px",
+            fontWeight: 400,
+          }}
+        >
+          Add Sub-units
+        </Button>
+      </Box>
+
+      {/* Add table here */}
+      <Box sx={{ mt: 2 }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="units table">
+            <TableHead>
+              <TableRow>
+                <TableCell>S/N</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {units.map((unit, index) => (
+                <TableRow key={unit.id}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{unit.name}</TableCell>
+                  <TableCell>{unit.description}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
 
       <CustomSpeedDial actions={[]} />
     </div>
